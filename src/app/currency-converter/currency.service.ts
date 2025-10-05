acafd685ba5664b81536dff959ff32c0adef2c4a
GitJacek <jack.rasala@gmail.com>
2025-10-05 13:30:01 +0200

    Catch errors

--- src/app/currency-converter/currency.service.ts
+++ src/app/currency-converter/currency.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Currency } from '../models/currency';
 import { ConvertCurrencyPayload } from '../models/convert-currency-payload';
 
@@ -13,16 +13,28 @@ export class CurrencyService {
   private httpClient = inject(HttpClient);
 
   getCurrencies = (): Observable<Currency[]> =>
-    this.httpClient
-      .get(`${this.baseUrl}/currencies?type=fiat`)
-      .pipe(map((response: any) => this.sortCurrencies(response.response)));
+    this.httpClient.get(`${this.baseUrl}/currencies?type=fiat`).pipe(
+      map((response: any) => this.sortCurrencies(response.response)),
+      catchError((error) => {
+        console.error(error);
+        return of([]);
+      })
+    );
 
   convert = (payload: ConvertCurrencyPayload): Observable<string> =>
     this.httpClient
       .get(
-        `${this.baseUrl}/convert?from=${payload.fromCurrency!.short_code}&to=${payload.toCurrency!.short_code}&amount=${payload!.amount}`
+        `${this.baseUrl}/convert?from=${payload.fromCurrency!.short_code}&to=${
+          payload.toCurrency!.short_code
+        }&amount=${payload!.amount}`
       )
-      .pipe(map((response: any) => response.response.value));
+      .pipe(
+        map((response: any) => response.response.value),
+        catchError((error) => {
+          console.error(error);
+          return of('');
+        })
+      );
 
   private sortCurrencies = (currencies: Currency[]): Currency[] =>
     currencies.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
